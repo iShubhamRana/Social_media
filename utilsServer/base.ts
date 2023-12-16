61052c16eb3d981edd591173ca06ce0ed23855ce
iShubhamRana <rana2001shubham@gmail.com>
2023-12-16 12:00:51 +0530

    addingprodurl

--- utilsServer/base.ts
+++ utilsServer/base.ts
@@ -1,2 +1,2 @@
-const baseUrl = "http://localhost:3000";
+const baseUrl = (process.env.NODE_ENV=="production")?"https://social-media-mamu.onrender.com":"http://localhost:3000";
 export default baseUrl;
